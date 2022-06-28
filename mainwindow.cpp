733b8d77c4da420e2bd4a2ba122652fe4fe86cbf
NhutThi2404 <nhutth2404@gmail.com>
2022-06-28 20:57:48 +0700

    add search scene

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,25 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->comboBoxSearch->addItem("Eng - Eng");
+    ui->comboBoxSearch->addItem("Viet - Eng");
+    ui->comboBoxSearch->addItem("Eng - Viet");
+    ui->comboBoxSearch->addItem("Slang");
+    ui->comboBoxSearch->addItem("Emoji");
+    ui->comboBoxSearch->addItem("Favourite");
+    this->setFixedHeight(544);
+    this->setFixedWidth(960);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+
+
 private:
     Ui::MainWindow *ui;
 };
