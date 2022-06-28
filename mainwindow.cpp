b051bc931ac09aa2c027eba2aa221d26a566830c
ggnohope <lamhoa9a1thcsttlp@gmail.com>
2022-06-28 22:14:52 +0700

    add csv function

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,47 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QMessageBox>
+#include <qpixmap.h>
+#include <iostream>
+#include <string>
+#include <QString>
+#include <QFile>
+#include "CSV.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QFile fin(":/data/data/Eng-Vie.csv");
+
+    if (!fin.open(QFile::ReadOnly | QFile::Text)) {
+        qDebug() << "Could not open file";
+    }
+
+    QTextStream in(&fin);
+    QString test = in.readLine();
+    QString word, def;
+    CSV::readLine(test, word, def);
+
+    ui->label->setText(word);
+    ui->label_2->setText(def);
+
+    fin.close();
+
+    QFile fout(":/data/data/output.txt");
+
+    if (!fout.open(QFile::WriteOnly | QFile::Text)) {
+        qDebug() << "Could not open file";
+    }
+
+    QTextStream out(&fout);
+
+    out << word << " "  << def;
+    fout.flush();
+    fout.close();
 }
 
 MainWindow::~MainWindow()
