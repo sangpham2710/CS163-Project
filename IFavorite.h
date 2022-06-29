a42a8590571a01494525251f530631274d6796b5
sangpham2710 <tansang.27.10.2003@gmail.com>
2022-06-29 08:23:24 +0700

    edit IFavorite

--- IFavorite.h
+++ IFavorite.h
@@ -3,13 +3,13 @@
 
 #include <QList>
 #include <QString>
-#include "FavoriteWord.h"
 
 class IFavorite {
 public:
-    virtual bool addWord(const FavoriteWord& word) = 0;
+    virtual bool addWord(const QString& word, const QString& definition) = 0;
     virtual bool removeWord(const QString& word) = 0;
-    virtual QList<FavoriteWord> getListFavoriteWords() = 0;
+    virtual QList<QString> getFavoriteWords() = 0;
+    virtual QString getFavoriteWordDefinition(const QString& word) = 0;
 };
 
 #endif // IFAVORITE_H
